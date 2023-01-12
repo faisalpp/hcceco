2ebe298e8375036d55ba5abd552187cac491f62b
faisalpp <muhammadfaisal522@gmail.com>
2023-01-12 15:46:17 +0500

    Vite Image Problem Not Solved

--- components/HomeComponents/PostEvent.jsx
+++ components/HomeComponents/PostEvent.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { BsPlusSquareFill } from 'react-icons/bs'
-import avtar from '../../src/images/avtar.png'
-
 
 const PostEvent = () => {
   
@@ -18,19 +16,19 @@ const PostEvent = () => {
        {/* Event Card */}
         <div className='relative w-42 h-36'>
          <div className='absolute top-1 z-10  text-center  text-white'><span className='ml-1 text-xs hover:underline cursor-pointer '>Manika</span><span className='relative top-1 left-14 bg-t3 w-5 h-5 px-1 py-1 rounded-full text-[10px]'>+5</span></div>
-          <img id="avtar" src={avtar} className='brightness-50 w-32 h-32 rounded-md'/>
+          <img id="avtar" src="/public/avtar.png" className='brightness-50 w-32 h-32 rounded-md'/>
         </div>
         {/* Event Card END */}
        {/* Event Card */}
         <div className='relative w-42 h-36'>
          <div className='absolute top-1 z-10  text-center  text-white'><span className='ml-1 text-xs hover:underline cursor-pointer '>Manika</span><span className='relative top-1 left-14 bg-t3 w-5 h-5 px-1 py-1 rounded-full text-[10px]'>+5</span></div>
-          <img id="avtar" src={avtar} className='brightness-50 w-32 h-32 rounded-md'/>
+          <img id="avtar" src="/public/avtar.png" className='brightness-50 w-32 h-32 rounded-md'/>
         </div>
         {/* Event Card END */}
        {/* Event Card */}
         <div className='relative w-42 h-36'>
          <div className='absolute top-1 z-10  text-center  text-white'><span className='ml-1 text-xs hover:underline cursor-pointer '>Manika</span><span className='relative top-1 left-14 bg-t3 w-5 h-5 px-1 py-1 rounded-full text-[10px]'>+5</span></div>
-          <img id="avtar" src={avtar} className='brightness-50 w-32 h-32 rounded-md'/>
+          <img id="avtar" src="/public/avtar.png" className='brightness-50 w-32 h-32 rounded-md'/>
         </div>
         {/* Event Card END */}
     </div>
